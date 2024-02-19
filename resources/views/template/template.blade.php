7e3233f1c1448ce14f6b83d6912f4851c8ee6f8d
Dicky_asqaelani26 <havidzgojek@gmail.com>
2024-02-19 17:19:45 +0700

    update navbar

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -7,34 +7,39 @@
     <title>@yield('title')</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-            <span class="navbar-toggler-icon"></span>
-         </button>
-         <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menu</h5>
-            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-         </div>
-         <div class="offcanvas-body">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                   <a href="#" class="nav-link">Beranda</a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{url('/barang/')}}" class="nav-link">Data Barang</a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{url('/jual/')}}" class="nav-link">Jual</a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{url('/beli/')}}" class="nav-link">Beli</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link"></a>
-                </li>
-            </ul>
-         </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menu</h5>
+                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/barang/')}}">Data Barang</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/jual/')}}">Jual</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/beli/')}}">Beli</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/stok/')}}">Stok</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Logs</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </nav>
 
